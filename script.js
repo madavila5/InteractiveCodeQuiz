5d61d37c75412133f540623aa11c2f2082da7a77
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-09-11 11:20:23 -0700

    Update const questions and start of structure, added timer div

--- script.js
+++ script.js
@@ -1,13 +1,90 @@
 
+const startbtn = document.getElementById("start")
+const nextbtn = document.getElementById('next')
+const qcontainerEl = document.getElementById('qcontainer')
+const qElement = document.getElementById('question')
+const ansElement = document.getElementById ('answerbtn');
 
-function start {
+let qShuffle, qIndex
+document.getElementById('start').addEventListener('click', start);
+nextbtn.addEventListener('click',()=> {
+    qIndex++
+    nextQ()
+} )
+
+//this function is to make the game start
+function start(){
+    startbtn.classList.add('hide')
+    qShuffle = questions.sort(() => Math.random()-.5)
+    qIndex = 0
+    document.getElementById('qContainer').classList.remove('hide')
+    nextQ()
+}
+//this function should set up the next question
+function nextQ (){
+    reset()
+    qVisible(qShuffle[qIndex])
 
 }
 
-function nextQ{
-
+function qVisible(question){
+    qElement.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document.createElement ('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click',ans)
+        ansElement.appendChild(button)
+    });
 }
-
-function ans{
-
-}
+//deleted section here because it was not working
+const questions = [
+    {
+        question: "What is the correct way to connect script?",
+        answer: [
+            {text:"<script src='script.js'></script>",correct: true},
+            {text:"<style src='script.js'></style>",correct: false},
+            {text:"<link src='script.js'></link>",correct: false},
+            {text:"connect src='script.js'></connect>",correct: false},
+        ]
+    },
+    {
+        question: "What is the correct way to connect stylesheet?",
+        answer: [
+            {text:"<link href='style.css' rel='stylesheet'>",correct: true},
+            {text:"<link href=style.css rel='stylesheet'>",correct: false},
+            {text:"<connect href='style.css' rel=stylesheet>",correct: false},
+            {text:"<link rel=stylesheet>",correct: false},
+        ]
+    },
+    {
+        question: "What does CSS stand for?",
+        answer: [
+            {text:"Cascading Style Sheet",correct: true},
+            {text:"Computer Style Sheet",correct: false},
+            {text:"Computer Sample Sheet",correct: false},
+            {text:"Cascading Sample Sheet",correct: false},
+        ]
+    },
+    {
+        question: "What does HTML stand for?",
+        answer: [
+            {text:"Hyper Text Markup Language",correct: true},
+            {text:"Hyperlink Tag Mark Language",correct: false},
+            {text:"Hyper Tag Madeup Language",correct: false},
+            {text:"Hyperlink Text Madeup Language",correct: false},
+        ]
+    },
+    {
+        question: "What does div tag define?",
+        answer: [
+            {text:"division",correct: true},
+            {text:"divident",correct: false},
+            {text:"divice",correct: false},
+            {text:"divoid",correct: false},
+        ]
+    }
+]
